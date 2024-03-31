55d9ecb367bcbce92ce6c1411de2b9e14cb451ee
khuluqilkarim <mpherex24@gmail.com>
2024-04-01 05:33:45 +0700

    updates

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ pip install -r requirements.txt
 directory=$(pwd)
 echo "#!/bin/bash" > grading
 echo "path=$directory" >> grading
-curl https://pastebin.com/raw/uwgUWK71 >> grading
+curl https://raw.githubusercontent.com/khuluqilkarim/grading/master/update/update.txt >> grading
 sed -i 's/\r//' grading
 chmod +x grading
 sudo mv grading /bin/grading
